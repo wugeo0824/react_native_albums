ee2733802a18dcc5176e47503e5ddc9464213424
wugeo0824 <xijun824@gmail.com>
2017-12-29 16:54:15 +0800

    add a header

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// import { AppRegistry } from 'react-native';
-// import App from './App';
-//
-// AppRegistry.registerComponent('albums', () => App);
-
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => {
+  return <Header headerText="Albums" />;
+};
+
+AppRegistry.registerComponent('albums', () => App);
